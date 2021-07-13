9d88bc5b7101dd3a085925a8f640268b7059b07c
Mudit Mittal <muditmittal2007@gmail.com>
2021-07-13 22:17:45 +1000

    final documentation

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-Steps to run the package:
+Package components:
+
+abnamro (parent directory)
+    -- coding_challenge -> contains all the instruction files provided initially
+    -- data
+        -- input.txt -> contains the initial raw data provided
+        -- specifications.txt -> in order to make the code more reusable, "specifications.txt" file has been created from the "System A File Specification.pdf"
+    -- examples
+        -- scratchpad1 -> playground to test out various logics
+        -- scratchpad2 -> playground to test out various logics
+    -- tests
+        -- test_columns.py -> contains 2 tests to validate the code
+    -- txt2csv
+        -- __init__.py -> empty package file
+        -- txt2output.py -> main code file containing the class
+    -- LICENSE -> license file
+    -- log -> log file generated from txt2output.py
+    -- Output.csv -> output file generated from txt2output.py
+    -- README.md -> contains details about the code package
+    -- setup.py -> contains the setup tool configurations
+
+Steps to install and run the package:
 
 1) Inside the package directory run the following command to install the setup file.
     pip3 install -e .
--- txt2csv/txt2output.py
+++ txt2csv/txt2output.py
@@ -1,17 +1,26 @@
+"""
+Python code to convert the input.txt file to output.csv file
+
+pip3 install -e .
+python txt2output.py
+python test_columns.py
+
+"""
 import pandas as pd
 import operator
 import logging
 
-
+#class to perform the necessary transformations and then store the data in output csv using its reusable methods
 class Txt2csv:
     def __init__(self, input_file_name, specs_file_name):
-        self.df = pd.read_csv(input_file_name, names=['Input'])
-        self.specs_file_name = specs_file_name
-        self.start_idx = None
-        self.last_idx = None
+        self.df = pd.read_csv(input_file_name, names=['Input']) #read the input.txt file as a pandas dataframe
+        self.specs_file_name = specs_file_name #input the specifications.txt file name
+        self.start_idx = None #slicing index start value
+        self.last_idx = None #slicing index last value
         self.txt2df()
 
-    def txt2df(self):
+    #create new columns in the dataframe based on the column name and length specified in the specifications.txt file
+    def txt2df(self): 
         try:
             with open(self.specs_file_name, "r") as specs:
                 for line in specs:
@@ -30,16 +39,17 @@ class Txt2csv:
         except:
             logging.error(f"Failed to create columns from {self.specs_file_name}!")
 
+    #add new column according to the input arithmatic operator 
     def addCols(self, addcols_dict):
         ops = {'+': operator.add, '-': operator.sub, 'x': operator.mul}
         for key,value in addcols_dict.items():
-            # try:
+            try:
                 self.df[key] = ops[value[1]](self.df[value[0]].astype('float'), self.df[value[2]].astype('float'))
-                # logging.debug(f'{key} column successfully added!')
-            # except:
-            #     logging.error(f'Failed to create {key} column!')
+                logging.debug(f'{key} column successfully added!')
+            except:
+                logging.error(f'Failed to create {key} column!')
 
-
+    #create output csv file based on the input parameters
     def createOutputFile(self, outputcols_dict, groupby_col, csv_headers):
             df_output_cols = []
             try:
@@ -56,13 +66,12 @@ class Txt2csv:
 
 if __name__ == '__main__':
 
-    # ops = {'+': operator.add, '-': operator.sub, 'x': operator.mul}
-    addcols_dict = {'Total_Transaction_Amount':['Quantity_Long', '-', 'Quantity_Short']}
+    addcols_dict = {'Total_Transaction_Amount':['Quantity_Long', '-', 'Quantity_Short']} #dictionary to add the column in the dataframe based on some operation
     outputcols_dict = {'Client_Information':['Client_Type', 'Client_Number', 'Account_Number', 'Subaccount_Number'],
                         'Product_Information':['Exchange_Code', 'Product_Group_Code', 'Symbol', 'Expiration_Date']
-                        }
-    output_csv_headers = ['Client_Information', 'Product_Information', 'Total_Transaction_Amount']
-    logging.basicConfig(level=logging.DEBUG, filename='log', format='%(asctime)s %(levelname)s:%(message)s')
-    t = Txt2csv('data/input.txt', 'data/specifications.txt')
-    t.addCols(addcols_dict)
-    t.createOutputFile(outputcols_dict, groupby_col='Total_Transaction_Amount', csv_headers = output_csv_headers)
+                        } #dictionary to add header names to the output csv
+    output_csv_headers = ['Client_Information', 'Product_Information', 'Total_Transaction_Amount'] #final output csv headers in order
+    logging.basicConfig(level=logging.DEBUG, filename='log', format='%(asctime)s %(levelname)s:%(message)s') #initialise log file and set its logging level
+    t = Txt2csv('data/input.txt', 'data/specifications.txt') #initialise the class
+    t.addCols(addcols_dict) #add the new column by calling the method from the class object
+    t.createOutputFile(outputcols_dict, groupby_col='Total_Transaction_Amount', csv_headers = output_csv_headers) #create the output csv file by calling the method from the class object
