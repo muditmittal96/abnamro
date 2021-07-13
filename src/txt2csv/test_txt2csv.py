b3f546608bd1f3145d646eceb336bb540774d7f8
Mudit Mittal <muditmittal2007@gmail.com>
2021-07-13 19:06:30 +1000

    second commit

--- src/txt2csv/test_txt2csv.py
+++ src/txt2csv/test_txt2csv.py
@@ -3,11 +3,20 @@ from txt2csv import *
 
 
 class TestTxt2csv(unittest.TestCase):
+    def setUp(self):
+        self.txt = Txt2csv('data/input.txt', 'data/specifications.txt')
+
+    def tearDown(self):
+        addcols_dict = {'Total_Transaction_Amount':['Quantity_Long', '-', 'Quantity_Short']}
+        self.txt.addCols(addcols_dict)
 
     def test_columns(self):
+        # print(self.txt.df.info())
+        self.assertIn('Total_Transaction_Amount',self.txt.df.columns)
 
-        t = Txt2csv('data/input.txt', 'data/specifications.txt')
-        addcols_dict = {'Total':['Quantity_long', '-', 'Quantity_Short']}
-        t.addCols(addcols_dict)
-        assert 'Total' in t.df.columns
+if __name__== "__main__":
+    unittest.main()
+    # test = TestTxt2csv()
+    # test.test_columns()
+    # print("All tests passed!")
     
